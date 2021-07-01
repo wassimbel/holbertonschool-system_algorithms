01f4a2097e1dcfebe0d3c3b8a5411edb8d49da5d
wassimbel <wassim.belhedi1@gmail.com>
2021-07-01 21:50:13 +0000

    fix betty

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -1,5 +1,13 @@
 #include "rb_trees.h"
 
+/**
+ * rb_tree_node -  creates a Red-Black Tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ * @color: the color of the node
+ * Return: returns a pointer to the new node, or NULL on failure
+ **/
+
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
 	rb_tree_t *new_tree;
