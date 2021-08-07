285ace2c0f9b3a3fa7b498e72b9b21d94d900c31
wassimbel <wassim.belhedi1@gmail.com>
2021-08-07 13:48:29 +0000

    function that adds an edge between two vertices to an existing graph

---
+++ 0x01-graphs/2-graph_add_edge.c
@@ -0,0 +1,98 @@
+#include "graphs.h"
+
+/**
+ * vertex_pt - returns a pointer to vertex containing s
+ * @graph: pointer to graph
+ * @s: pointer to string
+ * Return: returns a pointer to found vertex or NULL on failure
+ **/
+
+vertex_t *vertex_pt(graph_t *graph, const char *s)
+{
+	vertex_t *current;
+
+	current = graph->vertices;
+
+	while (current)
+	{
+		if (strcmp(current->content, s) == 0)
+			return (current);
+		current = current->next;
+	}
+	return (NULL);
+}
+
+/**
+ * add_edge - add an edge between 2 vertices
+ * @v_src: pointer to src vertex
+ * @v_dest: pointer to dest vertex
+ * Return: returns 1 on success otherwise 0
+ **/
+
+int add_edge(vertex_t *v_src, vertex_t *v_dest)
+{
+	edge_t *current, *new_edge;
+
+	current = v_src->edges;
+
+	new_edge = NULL;
+
+	while (current && current->next)
+		current = current->next;
+	new_edge = malloc(sizeof(*new_edge));
+
+	if (!new_edge)
+		return (0);
+
+	new_edge->dest = v_dest;
+	new_edge->next = NULL;
+	if (!v_src->edges)
+	{
+		v_src->edges = new_edge;
+		return (1);
+	}
+	else
+	{
+		current->next = new_edge;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * graph_add_edge - adds an edge between two vertices to an existing graph
+ * @graph: pointer to the graph to add the edge to
+ * @src: string identifying the vertex to make the connection from
+ * @dest: string identifying the vertex to connect to
+ * @type: type of edge
+ * Return: returns 1 on success otheriwse 0
+ */
+
+int graph_add_edge(graph_t *graph, const char *src, const char *dest,
+			 edge_type_t type)
+{
+	vertex_t *v_src, *v_dest;
+
+	if (!graph || !src || !dest ||
+		 (type != UNIDIRECTIONAL && type != BIDIRECTIONAL))
+		return (0);
+
+	v_src = vertex_pt(graph, src);
+	v_dest = vertex_pt(graph, dest);
+	if (type == BIDIRECTIONAL)
+	{
+		if (!add_edge(v_src, v_dest))
+			return (0);
+		if (!add_edge(v_dest, v_src))
+			return (0);
+		v_src->nb_edges += 1;
+		v_dest->nb_edges += 1;
+	}
+	else
+	{
+		if (!add_edge(v_src, v_dest))
+			return (0);
+		v_src->nb_edges += 1;
+	}
+	return (1);
+}
---
+++ 0x01-graphs/graphs.h
@@ -0,0 +1,76 @@
+#ifndef _GRAPHS_H_
+#define _GRAPHS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * enum edge_type_e - Enumerates the different types of
+ * connection between two vertices
+ *
+ * @UNIDIRECTIONAL: The connection is made only in one way
+ * @BIDIRECTIONAL: The connection is made in two ways
+ */
+typedef enum edge_type_e
+{
+ 	UNIDIRECTIONAL = 0,
+ 	BIDIRECTIONAL
+} edge_type_t;
+
+/* Define the structure temporarily for usage in the edge_t */
+typedef struct vertex_s vertex_t;
+
+/**
+ * struct edge_s - Node in the linked list of edges for a given vertex
+ * A single vertex can have many edges
+ *
+ * @dest: Pointer to the connected vertex
+ * @next: Pointer to the next edge
+ */
+typedef struct edge_s
+{
+	vertex_t    *dest;
+	struct edge_s   *next;
+} edge_t;
+
+
+/**
+ * struct vertex_s - Node in the linked list of vertices in the adjacency list
+ *
+ * @index: Index of the vertex in the adjacency list.
+ * @content: Custom data stored in the vertex (here, a string)
+ * @nb_edges: Number of conenctions with other vertices in the graph
+ * @edges: Pointer to the head node of the linked list of edges
+ * @next: Pointer to the next vertex in the adjacency linked list
+ *   This pointer points to another vertex in the graph, but it
+ *   doesn't stand for an edge between the two vertices
+ */
+struct vertex_s
+{
+	size_t      index;
+	char        *content;
+	size_t      nb_edges;
+	edge_t      *edges;
+	struct vertex_s *next;
+};
+
+/**
+ * struct graph_s - Representation of a graph
+ * We use an adjacency linked list to represent our graph
+ *
+ * @nb_vertices: Number of vertices in our graph
+ * @vertices: Pointer to the head node of our adjacency linked list
+ */
+typedef struct graph_s
+{
+	size_t      nb_vertices;
+	vertex_t    *vertices;
+} graph_t;
+
+graph_t *graph_create(void);
+vertex_t *graph_add_vertex(graph_t *graph, const char *str);
+void graph_display(const graph_t *graph);
+int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type);
+int add_edge(vertex_t *v_src, vertex_t *v_dest);
+vertex_t *vertex_pt(graph_t *graph, const char *s);
+
+#endif /* _GRAPHS_H_ */
